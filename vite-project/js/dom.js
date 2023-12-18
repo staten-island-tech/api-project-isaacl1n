88de0951de8b66672535218b9e95b9dad97378d2
isaacl81 <isaaclin2007@gmail.com>
2023-12-17 21:50:06 -0500

    data can now be logged

--- vite-project/js/dom.js
+++ vite-project/js/dom.js
@@ -2,6 +2,5 @@ export const DOMselectors = {
     searchForm: document.getElementById("search"),
     searchSelector: document.getElementById("search-selector"),
     searchBar: document.getElementById("search-bar"),
-    searchButton: document.getElementById("search-button"),
     result: document.getElementById("result"),
 };
--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -2,29 +2,38 @@ import '../css/style.css'
 import { DOMselectors } from './dom.js'
 
 DOMselectors.searchForm.addEventListener("submit", function(event) {
-  event.preventDefault()
+  event.preventDefault();
   const category = DOMselectors.searchSelector.value.toString()
   const query = DOMselectors.searchBar.value
 
   const URL = "https://restcountries.com/v3.1/" + category + "/" + query
-  console.log(fetch(URL))
+  console.log(fetch(URL));
 
   async function getData(URL) {
     try {
       const response = await fetch(URL);
       const data = await response.json();
       console.log(data);
+
+      DOMselectors.result.innerHTML = "";
+      data.forEach(territory => {
+        console.log(territory.area);
+        DOMselectors.result.insertAdjacentHTML(
+          "beforeend",
+          `
+            
+          `
+        );
+      });
     } catch (error) {
       console.log(error);
-    }
-  }
+      DOMselectors.result.insertAdjacentHTML(
+        "beforeend",
+        `
+          
+        `
+        );
+    };
+  };
   getData(URL);
-
-  DOMselectors.result.innerHTML = "";
-  DOMselectors.result.insertAdjacentHTML(
-    "beforeend",
-    `
-      
-    `
-  );
 });
